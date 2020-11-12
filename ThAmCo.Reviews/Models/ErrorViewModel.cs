e24d7af7d161f638e6791bf0ce189ce3af4cfb9b
Patryk Neter <v8243060@tees.ac.uk>
2020-11-12 21:50:44 +0000

    Add project files.

---
+++ ThAmCo.Reviews/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ThAmCo.Reviews.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ThAmCo.Reviews/Models/ReviewDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThAmCo.Reviews.Models
+{
+    public class ReviewDto
+    {
+        public int reviewId { get; set; }
+
+        public int userId { get; set; }
+
+        public int productId { get; set; }
+
+        public string userName { get; set; }
+
+        public string reviewContent { get; set; }
+
+        public int reviewRating { get; set; }
+    }
+}
