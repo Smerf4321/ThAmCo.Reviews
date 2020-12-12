deb764a7e2870061066976176ca087a5cad51434
Patryk Neter <v8243060@tees.ac.uk>
2020-12-12 15:17:48 +0000

    Added some unit tests

--- ReviewTests/Controllers/ReviewControllerTest.cs
+++ ReviewTests/Controllers/ReviewControllerTest.cs
@@ -12,18 +12,19 @@ namespace ReviewTests
     [TestClass]
     public class ReviewControllerTest
     {
-        [TestMethod]
-        public async Task GetAllReviews_ShouldOKObject()
-        {
-
-        List<ReviewDto> reviews = new List<ReviewDto>
+        List<ReviewDto> _reviews = new List<ReviewDto>
         {
             new ReviewDto {reviewId = 1, productId = 1, userId = 1, userName = "Joe", reviewRating = 5, reviewContent = "Great Product." },
             new ReviewDto {reviewId = 2, productId = 2, userId = 2, userName = "Bob", reviewRating = 3, reviewContent = "It's okay." },
+            new ReviewDto {reviewId = 3, productId = 1, userId = 2, userName = "Bob", reviewRating = 1, reviewContent = "Really bad." },
+            new ReviewDto {reviewId = 4, productId = 3, userId = 1, userName = "Joe", reviewRating = 3, reviewContent = "It's okay." },
         };
 
-            var repo = new FakeReviewService(reviews);
-            var controller = new ReviewController(repo);
+        [TestMethod]
+        public async Task GetAllReviews_ShouldOKObject()
+        {
+            var service = new FakeReviewService(_reviews);
+            var controller = new ReviewController(service);
             var result = await controller.GetReviewListAsync(null, null);
 
 
@@ -33,16 +34,121 @@ namespace ReviewTests
             var reviewsResult = objResult.Value as IEnumerable<ReviewDto>;
             Assert.IsNotNull(reviewsResult);
             var reviewsResultList = reviewsResult.ToList();
-            Assert.AreEqual(reviews.Count, reviewsResultList.Count);
-            for (int i = 0; i < reviews.Count; ++i)
+            Assert.AreEqual(_reviews.Count, reviewsResultList.Count);
+            for (int i = 0; i < _reviews.Count; ++i)
             {
-                Assert.AreEqual(reviews[i].reviewId, reviewsResultList[i].reviewId);
-                Assert.AreEqual(reviews[i].productId, reviewsResultList[i].productId);
-                Assert.AreEqual(reviews[i].userId, reviewsResultList[i].userId);
-                Assert.AreEqual(reviews[i].userName, reviewsResultList[i].userName);
-                Assert.AreEqual(reviews[i].reviewRating, reviewsResultList[i].reviewRating);
-                Assert.AreEqual(reviews[i].reviewContent, reviewsResultList[i].reviewContent);
+                Assert.AreEqual(_reviews[i].reviewId, reviewsResultList[i].reviewId);
+                Assert.AreEqual(_reviews[i].productId, reviewsResultList[i].productId);
+                Assert.AreEqual(_reviews[i].userId, reviewsResultList[i].userId);
+                Assert.AreEqual(_reviews[i].userName, reviewsResultList[i].userName);
+                Assert.AreEqual(_reviews[i].reviewRating, reviewsResultList[i].reviewRating);
+                Assert.AreEqual(_reviews[i].reviewContent, reviewsResultList[i].reviewContent);
             }
+        }
+
+        [TestMethod]
+        public async Task GetAllReviewsForProductId_ShouldOKObject()
+        {
+            var service = new FakeReviewService(_reviews);
+            var controller = new ReviewController(service);
+            var result = await controller.GetReviewListAsync(1, null);
+
+
+            Assert.IsNotNull(result);
+            var objResult = result as OkObjectResult;
+            Assert.IsNotNull(objResult);
+            var reviewsResult = objResult.Value as IEnumerable<ReviewDto>;
+            Assert.IsNotNull(reviewsResult);
+            Assert.IsTrue(reviewsResult.All(r => r.productId == 1));
+            var reviewsResultList = reviewsResult.ToList();
+            var limitedReviews = _reviews.FindAll(r => r.productId == 1);
+            Assert.AreEqual(limitedReviews.Count, reviewsResultList.Count);
+
+            
+
+            for (int i = 0; i < limitedReviews.Count; ++i)
+            {
+                Assert.AreEqual(limitedReviews[i].reviewId, reviewsResultList[i].reviewId);
+                Assert.AreEqual(limitedReviews[i].productId, reviewsResultList[i].productId);
+                Assert.AreEqual(limitedReviews[i].userId, reviewsResultList[i].userId);
+                Assert.AreEqual(limitedReviews[i].userName, reviewsResultList[i].userName);
+                Assert.AreEqual(limitedReviews[i].reviewRating, reviewsResultList[i].reviewRating);
+                Assert.AreEqual(limitedReviews[i].reviewContent, reviewsResultList[i].reviewContent);
+            }
+        }
+
+        [TestMethod]
+        public async Task GetAllReviewsForUserId_ShouldOKObject()
+        {
+            var service = new FakeReviewService(_reviews);
+            var controller = new ReviewController(service);
+            var result = await controller.GetReviewListAsync(null, 1);
+
+
+            Assert.IsNotNull(result);
+            var objResult = result as OkObjectResult;
+            Assert.IsNotNull(objResult);
+            var reviewsResult = objResult.Value as IEnumerable<ReviewDto>;
+            Assert.IsNotNull(reviewsResult);
+            Assert.IsTrue(reviewsResult.All(r => r.userId == 1));
+            var reviewsResultList = reviewsResult.ToList();
+            var limitedReviews = _reviews.FindAll(r => r.userId == 1);
+            Assert.AreEqual(limitedReviews.Count, reviewsResultList.Count);
+
+            for (int i = 0; i < limitedReviews.Count; ++i)
+            {
+                Assert.AreEqual(limitedReviews[i].reviewId, reviewsResultList[i].reviewId);
+                Assert.AreEqual(limitedReviews[i].productId, reviewsResultList[i].productId);
+                Assert.AreEqual(limitedReviews[i].userId, reviewsResultList[i].userId);
+                Assert.AreEqual(limitedReviews[i].userName, reviewsResultList[i].userName);
+                Assert.AreEqual(limitedReviews[i].reviewRating, reviewsResultList[i].reviewRating);
+                Assert.AreEqual(limitedReviews[i].reviewContent, reviewsResultList[i].reviewContent);
+            }
+        }
+
+        [TestMethod]
+        public async Task GetReviewWithReviewId_ShouldOKObject()
+        {
+            var service = new FakeReviewService(_reviews);
+            var controller = new ReviewController(service);
+            var result = await controller.GetReviewAsync(1);
+
+
+            Assert.IsNotNull(result);
+            var objResult = result as OkObjectResult;
+            Assert.IsNotNull(objResult);
+            var reviewsResult = objResult.Value as ReviewDto;
+            Assert.IsNotNull(reviewsResult);
+
+            var targetReview = _reviews.Find(r => r.reviewId == 1);
+
+            Assert.AreEqual(targetReview.productId, reviewsResult.productId);
+            Assert.AreEqual(targetReview.userId, reviewsResult.userId);
+            Assert.AreEqual(targetReview.userName, reviewsResult.userName);
+            Assert.AreEqual(targetReview.reviewRating, reviewsResult.reviewRating);
+            Assert.AreEqual(targetReview.reviewContent, reviewsResult.reviewContent);
+        }
+
+        public async Task Create_ShouldOKObject()
+        { 
+            var service = new FakeReviewService(_reviews);
+            var controller = new ReviewController(service);
+            var result = await controller.GetReviewAsync(1);
+
+
+            Assert.IsNotNull(result);
+            var objResult = result as OkObjectResult;
+            Assert.IsNotNull(objResult);
+            var reviewsResult = objResult.Value as ReviewDto;
+            Assert.IsNotNull(reviewsResult);
+
+            var targetReview = _reviews.Find(r => r.reviewId == 1);
+
+            Assert.AreEqual(targetReview.productId, reviewsResult.productId);
+            Assert.AreEqual(targetReview.userId, reviewsResult.userId);
+            Assert.AreEqual(targetReview.userName, reviewsResult.userName);
+            Assert.AreEqual(targetReview.reviewRating, reviewsResult.reviewRating);
+            Assert.AreEqual(targetReview.reviewContent, reviewsResult.reviewContent);
         }
     }
 }
--- ReviewTests/Services/FakeReviewServiceTest.cs
+++ ReviewTests/Services/FakeReviewServiceTest.cs
@@ -12,33 +12,84 @@ namespace ReviewTests.Services
     [TestClass]
     public class FakeReviewServicesTest
     {
-        [TestMethod]
-        public async Task GetAllReviews_ShouldOKObject()
-        {
-
-            List<ReviewDto> reviews = new List<ReviewDto>
+        List<ReviewDto> _reviews = new List<ReviewDto>
         {
             new ReviewDto {reviewId = 1, productId = 1, userId = 1, userName = "Joe", reviewRating = 5, reviewContent = "Great Product." },
             new ReviewDto {reviewId = 2, productId = 2, userId = 2, userName = "Bob", reviewRating = 3, reviewContent = "It's okay." },
+            new ReviewDto {reviewId = 3, productId = 1, userId = 2, userName = "Bob", reviewRating = 1, reviewContent = "Really bad." },
+            new ReviewDto {reviewId = 4, productId = 3, userId = 1, userName = "Joe", reviewRating = 3, reviewContent = "It's okay." },
         };
 
-            var repo = new FakeReviewService(reviews);
-            var result = await repo.GetReviewListAsync(null, null);
-
+        [TestMethod]
+        public async Task GetAllReviews_ShouldOKObject()
+        {
+            var service = new FakeReviewService(_reviews);
+            var result = await service.GetReviewListAsync(null, null);
 
             Assert.IsNotNull(result);
-            var objResult = result as IEnumerable<ReviewDto>;
-            Assert.IsNotNull(objResult);
-            var reviewsResultList = objResult.ToList();
-            Assert.AreEqual(reviews.Count, reviewsResultList.Count);
-            for (int i = 0; i < reviews.Count; ++i)
+            var reviewsResult = result as IEnumerable<ReviewDto>;
+            Assert.IsNotNull(reviewsResult);
+            var resultList = reviewsResult.ToList();
+            Assert.AreEqual(_reviews.Count, resultList.Count);
+
+            for (int i = 0; i < _reviews.Count; ++i)
             {
-                Assert.AreEqual(reviews[i].reviewId, reviewsResultList[i].reviewId);
-                Assert.AreEqual(reviews[i].productId, reviewsResultList[i].productId);
-                Assert.AreEqual(reviews[i].userId, reviewsResultList[i].userId);
-                Assert.AreEqual(reviews[i].userName, reviewsResultList[i].userName);
-                Assert.AreEqual(reviews[i].reviewRating, reviewsResultList[i].reviewRating);
-                Assert.AreEqual(reviews[i].reviewContent, reviewsResultList[i].reviewContent);
+                Assert.AreEqual(_reviews[i].reviewId, resultList[i].reviewId);
+                Assert.AreEqual(_reviews[i].productId, resultList[i].productId);
+                Assert.AreEqual(_reviews[i].userId, resultList[i].userId);
+                Assert.AreEqual(_reviews[i].userName, resultList[i].userName);
+                Assert.AreEqual(_reviews[i].reviewRating, resultList[i].reviewRating);
+                Assert.AreEqual(_reviews[i].reviewContent, resultList[i].reviewContent);
+            }
+        }
+
+        [TestMethod]
+        public async Task GetAllReviewsForProductId_ShouldOKObject()
+        {
+            var service = new FakeReviewService(_reviews);
+            var result = await service.GetReviewListAsync(1, null);
+
+            Assert.IsNotNull(result);
+            var reviewsResult = result as IEnumerable<ReviewDto>;
+            Assert.IsNotNull(reviewsResult);
+            Assert.IsTrue(reviewsResult.All(r => r.productId == 1));
+            var reviewsResultList = reviewsResult.ToList();
+            var limitedReviews = _reviews.FindAll(r => r.productId == 1);
+            Assert.AreEqual(limitedReviews.Count, reviewsResultList.Count);
+
+            for (int i = 0; i < limitedReviews.Count; ++i)
+            {
+                Assert.AreEqual(limitedReviews[i].reviewId, reviewsResultList[i].reviewId);
+                Assert.AreEqual(limitedReviews[i].productId, reviewsResultList[i].productId);
+                Assert.AreEqual(limitedReviews[i].userId, reviewsResultList[i].userId);
+                Assert.AreEqual(limitedReviews[i].userName, reviewsResultList[i].userName);
+                Assert.AreEqual(limitedReviews[i].reviewRating, reviewsResultList[i].reviewRating);
+                Assert.AreEqual(limitedReviews[i].reviewContent, reviewsResultList[i].reviewContent);
+            }
+        }
+
+        [TestMethod]
+        public async Task GetAllReviewsForUserId_ShouldOKObject()
+        {
+            var service = new FakeReviewService(_reviews);
+            var result = await service.GetReviewListAsync(null, 1);
+
+            Assert.IsNotNull(result);
+            var reviewsResult = result as IEnumerable<ReviewDto>;
+            Assert.IsNotNull(reviewsResult);
+            Assert.IsTrue(reviewsResult.All(r => r.userId == 1));
+            var reviewsResultList = reviewsResult.ToList();
+            var limitedReviews = _reviews.FindAll(r => r.userId == 1);
+            Assert.AreEqual(limitedReviews.Count, reviewsResultList.Count);
+
+            for (int i = 0; i < limitedReviews.Count; ++i)
+            {
+                Assert.AreEqual(limitedReviews[i].reviewId, reviewsResultList[i].reviewId);
+                Assert.AreEqual(limitedReviews[i].productId, reviewsResultList[i].productId);
+                Assert.AreEqual(limitedReviews[i].userId, reviewsResultList[i].userId);
+                Assert.AreEqual(limitedReviews[i].userName, reviewsResultList[i].userName);
+                Assert.AreEqual(limitedReviews[i].reviewRating, reviewsResultList[i].reviewRating);
+                Assert.AreEqual(limitedReviews[i].reviewContent, reviewsResultList[i].reviewContent);
             }
         }
     }
